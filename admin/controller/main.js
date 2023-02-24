58d46847e1d8e436798d132b95ea4c2d0155e2e1
Nghia <nghia3977@gmail.com>
2023-02-24 11:14:32 +0700

    build the function of updating data

--- admin/controller/main.js
+++ admin/controller/main.js
@@ -5,8 +5,10 @@ getPhones();
 // Lấy data từ server
 async function getPhones(searchVal) {
     try {
+        // gọi API thành công
         const { data: phones } = await getPhoneAPI(searchVal);
         console.log(phones);
+        // hiển thị danh sách data lên UI
         renderPhones(phones);
     } catch (error) {
         // gọi API thất bại
@@ -27,13 +29,18 @@ getEle("#btnAdd").addEventListener("click", async () => {
         img: getEle("#img").value,
         desc: getEle("#desc").value,
         type: getEle("#type").value
-    }
+    };
 
     try {
         // gửi request tạo data mới cho server
         await generatePhoneAPI(phone);
+
+        // tắt form sau khi tạo xong data mới
+        getEle("#btnAdd").setAttribute("data-dismiss", "modal");
+
         // update lại danh sách sau khi tạo mới
         getPhones();
+
         // hiển thị thông báo tạo mới thành công
         Swal.fire({
             position: 'top-end',
@@ -42,14 +49,18 @@ getEle("#btnAdd").addEventListener("click", async () => {
             showConfirmButton: false,
             timer: 1500
         });
+
+        // xóa tất cả input trong form
+        resetForm("phoneForm");
     } catch (error) {
+        // tạo data mới thất bại
         console.log("Failed to add new data", error);
     }
 })
 
 
 // Xóa data bên server
-async function deletePhones(phoneId) {
+async function deletePhones(phoneID) {
     try {
         // khi click vào nút Delete thì sẽ hiển thị alert thông báo xác nhận có muốn xóa data không
         // sử dụng thư viện SweetAlert2 & gán thuộc tính isConfirmed cho biến result
@@ -63,9 +74,11 @@ async function deletePhones(phoneId) {
         // data sẽ chỉ được xóa khi và chỉ khi user click nút confirm (result === true)
         if (result) {
             // thực hiện xóa data
-            await deletePhoneAPI(phoneId);
+            await deletePhoneAPI(phoneID);
+
             // update lại danh sách sau khi xóa
             getPhones();
+
             // hiển thị thông báo xác nhận xóa thành công
             Swal.fire({
                 position: 'top-end',
@@ -76,7 +89,75 @@ async function deletePhones(phoneId) {
             });
         }
     } catch (error) {
+        // xóa data thất bại
         console.log("Failed to delete data", error);
+    }
+}
+
+
+// Hiển thị chi tiết 1 sản phẩm bất kì lên form sau khi click nút Edit
+async function selectPhone(phoneID) {
+    getEle("#btnAdd").style.display = "none";
+    getEle("#btnUpdate").style.display = "inline-block"
+
+    try {
+        // lấy data dựa theo ID từ server
+        const { data: phone } = await getPhoneAPIByID(phoneID);
+
+        // hiển thị thông tin của từng thuộc tính lên field của form
+        getEle("#name").value = phone.name;
+        getEle("#price").value = phone.price;
+        getEle("#screen").value = phone.screen;
+        getEle("#bCam").value = phone.backCamera;
+        getEle("#fCam").value = phone.frontCamera;
+        getEle("#img").value = phone.img;
+        getEle("#desc").value = phone.desc;
+        getEle("#type").value = phone.type;
+
+        // tạo thuộc tính onclick có value gọi hàm updatePhone với tham số là ID của data đã lấy
+        getEle("#btnUpdate").setAttribute("onclick", `updatePhone(${phone.id})`);
+
+    } catch (error) {
+        // lấy data bằng ID thất bại
+        console.log("Failed to get data by ID", error);
+    }
+}
+
+
+// Cập nhật thông tin data
+async function updatePhone(phoneID) {
+    const phone = {
+        name: getEle("#name").value,
+        price: getEle("#price").value,
+        screen: getEle("#screen").value,
+        backCamera: getEle("#bCam").value,
+        frontCamera: getEle("#fCam").value,
+        img: getEle("#img").value,
+        desc: getEle("#desc").value,
+        type: getEle("#type").value
+    };
+
+    try {
+        // gửi request cập nhật data cho server
+        await updatePhoneAPI(phoneID, phone);
+
+        // lấy danh sách data sau khi cập nhật & hiển thị lên UI
+        getPhones();
+
+        // hiển thị thông báo cập nhật thành công
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your data has been updated',
+            showConfirmButton: false,
+            timer: 1500
+        });
+
+        // xóa tất cả input trong form
+        resetForm("phoneForm");
+    } catch (error) {
+        // cập nhật data thất bại
+        console.log("Failed to update data", error);
     }
 }
 
@@ -93,13 +174,13 @@ function renderPhones(phones) {
                 <td class="text-center"><img src=${phone.img} with="150px" height="150px" alt="phone image"/></td>
                 <td>${phone.desc}</td>
                 <td class="text-center">
-                    <button class="btn btn-primary" data-toggle="modal" data-target="#phoneModal">Edit<i class="fa-regular fa-pen-to-square ml-2"></i></i></button>
+                    <button class="btn btn-primary" data-toggle="modal" data-target="#phoneModal" onclick="selectPhone(${phone.id})">Edit<i class="fa-regular fa-pen-to-square ml-2"></i></i></button>
                     <button class="btn btn-danger ml-2" onclick="deletePhones(${phone.id})">Delete<i class="fa-regular fa-trash-can ml-2"></i></button>
                 </td>
             </tr>
             `
         );
-    }, "")
+    }, "");
 
     getEle("#phoneList").innerHTML = html;
 }
@@ -116,12 +197,15 @@ getEle("#txtSearch").addEventListener("keydown", e => {
 })
 
 getEle("#btnOpenForm").addEventListener("click", () => {
-    // khi click mở form thì ẩn nút Update
     getEle("#btnUpdate").style.display = "none";
+    getEle("#btnAdd").style.display = "inline-block";
 })
-
 
 // HELPERS
 function getEle(selector) {
     return document.querySelector(selector);
+}
+
+function resetForm(formID) {
+    return document.getElementById(formID).reset();
 }
--- admin/services/phoneAPI.js
+++ admin/services/phoneAPI.js
@@ -1,31 +1,72 @@
 const BASE_URL = "https://63e86417cbdc565873852d8b.mockapi.io/api/products";
 
-// Lấy data từ phía server
-function getPhoneAPI(searchVal) {
-    return axios({
-        method: "GET",
-        url: BASE_URL,
-        params: {
-            name: searchVal || undefined
-        }
-    });
+// Lấy tất cả data từ server
+async function getPhoneAPI(searchVal) {
+    try {
+        const response = await axios({
+            method: "GET",
+            url: BASE_URL,
+            params: {
+                name: searchVal || undefined
+            }
+        });
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
 // Tạo mới data & lưu vào server
-function generatePhoneAPI(phone) {
-    return axios({
-        method: "POST",
-        url: BASE_URL,
-        data: phone
-    });
+async function generatePhoneAPI(phone) {
+    try {
+        await axios({
+            method: "POST",
+            url: BASE_URL,
+            data: phone
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
 // Xóa data bên phía server
-function deletePhoneAPI(phoneId) {
-    return axios({
-        method: "DELETE",
-        url: `${BASE_URL}/${phoneId}`
-    });
+async function deletePhoneAPI(phoneID) {
+    try {
+        await axios({
+            method: "DELETE",
+            url: `${BASE_URL}/${phoneID}`
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// Lấy data dựa theo id
+async function getPhoneAPIByID(phoneID) {
+    try {
+        const response = await axios({
+            method: "GET",
+            url: `${BASE_URL}/${phoneID}`
+        });
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// Cập nhật data cho server
+async function updatePhoneAPI(phoneID, phone) {
+    try {
+        await axios({
+            method: "PUT",
+            url: `${BASE_URL}/${phoneID}`,
+            data: phone
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
